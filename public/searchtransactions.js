7ff79c69e7d5f26fe97ec9ea8a36017dd3c11bce
ErinTanaka <tanakae@flip3.engr.oregonstate.edu>
2018-11-21 11:44:28 -0800

    added search functionality

--- public/searchtransactions.js
+++ public/searchtransactions.js
@@ -1,6 +1,6 @@
-function searchTransactionbyCname() {
-    //get the first name 
+function searchTransactionsByCame() {
+    //get the cname
     var cname_search_string  = document.getElementById('cname_search_string').value
     //construct the URL and redirect to it
-    window.location = '/transactions/search/' + encodeURI(cname_search_string)
+    window.location = '/transactions/search_transactions/' + encodeURI(cname_search_string)
 }
--- transactions.js
+++ transactions.js
@@ -22,41 +22,25 @@ module.exports = function () {
            complete();
        });
      }
+     function getTransactionsByCname(req, res, mysql, context, complete){
+        var query = "(SELECT tr.id AS 'ID', tr.cname AS 'Name', an.type AS 'Item' FROM transaction_table tr INNER JOIN adoption_table ad ON tr.id = ad.tid INNER JOIN animal_table an ON ad.aid = an.id WHERE cname = " + mysql.pool.escape(req.params.s) + ") UNION ALL (SELECT tr2.id AS 'ID', tr2.cname AS 'Name', su.item AS 'Item' FROM transaction_table tr2 INNER JOIN purchase_table pr ON tr2.id = pr.tid INNER JOIN supply_table su ON pr.sid=su.id WHERE cname = " +  mysql.pool.escape(req.params.s) + ")";
+        mysql.pool.query(query, function(error, results, fields){
+          if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+          }
+          context.transactions = results;
+          complete();
+        });
+      }
 
-    // function getTransactionSuppliesByCname(res, mysql, context, complete) {
-    //     var query = "SELECT purchase_table.tid AS 'id', transaction_table.cname AS 'cname', supply_table.bname AS 'item', animal_table.type FROM purchase_table INNER JOIN transaction_table ON purchase_table.tid = transaction_table.id INNER JOIN supply_table ON purchase_table.sid = supply_table.id";
-    //     console.log(req.params)
-    //     var inserts = [req.params.transactionSupply]
-    //     mysql.pool.query(query, inserts, function (error, results, fields) {
-    //         if (error) {
-    //             res.write(JSON.stringify(error));
-    //             res.end();
-    //         }
-    //         context.transaction = results;
-    //         complete();
-    //     });
-    // }
-    //
-    // function getTransactionAnimalsByCname(res, mysql, context, complete) {
-    //     var query = "SELECT adoption_table.tid AS 'id', transaction_table.cname AS 'cname', animal_table.type AS 'item' FROM adoption_table INNER JOIN transaction_table ON adoption_table.tid = transaction_table.id INNER JOIN animal_table ON adoption_table.aid = animal_table.id";
-    //     console.log(req.params)
-    //     var inserts = [req.params.transactionAnimal]
-    //     mysql.pool.query(query, inserts, function (error, results, fields) {
-    //         if (error) {
-    //             res.write(JSON.stringify(error));
-    //             res.end();
-    //         }
-    //         context.transaction = results;
-    //         complete();
-    //     });
-    // }
 
     /*add new animal transaction*/
 
     router.get('/', function (req, res) {
         var callbackCount = 0;
         var context = {};
-        context.jsscripts = []; //"deleteperson.js","filterpeople.js","searchpeople.js"
+        context.jsscripts = ["searchtransactions.js"]; //"deleteperson.js","filterpeople.js","searchpeople.js"
         var mysql = req.app.get('mysql');
         getAllTransactionshopefully(res, mysql, context, complete);
         //getTransactionSuppliesByCname(res, mysql, context, complete);
@@ -68,8 +52,64 @@ module.exports = function () {
             }
         }
     });
+
+    router.get('/transactions/returnsupply', function (req, res) {
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["searchtransactions.js"];
+        var mysql = req.app.get('mysql');
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "(SELECT tr.id AS 'ID', tr.cname AS 'Name', an.type AS 'Item' FROM transaction_table tr INNER JOIN adoption_table ad ON tr.id = ad.tid INNER JOIN animal_table an ON ad.aid = an.id WHERE cname=?) UNION ALL (SELECT tr2.id AS 'ID', tr2.cname AS 'Name', su.item AS 'Item' FROM transaction_table tr2 INNER JOIN purchase_table pr ON tr2.id = pr.tid INNER JOIN supply_table su ON pr.sid=su.id WHERE cname=?) ";
+        var inserts = [req.body.cname, req.body.cname];
+        sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.transactions = results;
+            complete();
+        });
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('transactions', context);
+            }
+        }
+    });
+    /*Display all people whose name starts with a given string. Requires web based javascript to delete users with AJAX */
+    router.get('/search_transactions/:s', function(req, res){
+      console.log("search fn")
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["searchtransactions.js"];
+        var mysql = req.app.get('mysql');
+        getTransactionsByCname(req, res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('transactions', context);
+            }
+        }
+    });
+
+    /*Display all people whose name starts with a given string. Requires web based javascript to delete users with AJAX */
+    // router.get('/search/:s', function(req, res){
+    //     var callbackCount = 0;
+    //     var context = {};
+    //     context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+    //     var mysql = req.app.get('mysql');
+    //     getPeopleWithNameLike(req, res, mysql, context, complete);
+    //     getPlanets(res, mysql, context, complete);
+    //     function complete(){
+    //         callbackCount++;
+    //         if(callbackCount >= 2){
+    //             res.render('people', context);
+    //         }
+    //     }
+    // });
     router.post('/addtrans', function (req, res) {
-        //console.log(req.body.homeworld)
         console.log("made it in my fn");
         console.log(req.body);
         console.log(req.body.type)
@@ -85,12 +125,7 @@ module.exports = function () {
                   res.write(JSON.stringify(error));
                   res.end();
               }else {
-                //console.log("eeeeeeeeeeeh");
-                //console.log(results.insertId);
-                //console.log("fuck");
                 var temp = results.insertId;
-                //res.redirect('/transactions');
-                //var mysql = req.app.get('mysql');
                 sql = "INSERT INTO `adoption_table` (`tid`, `aid`) VALUES (?, ?);";
                 inserts = [temp, req.body.item];
                 sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
@@ -116,12 +151,7 @@ module.exports = function () {
                   res.write(JSON.stringify(error));
                   res.end();
               }else {
-                //console.log("eeeeeeeeeeeh");
-                //console.log(results.insertId);
-                //console.log("fuck");
                 var temp = results.insertId;
-                //res.redirect('/transactions');
-                //var mysql = req.app.get('mysql');
                 sql = "INSERT INTO `purchase_table` (`tid`, `sid`) VALUES (?, ?);";
                 inserts = [temp, req.body.item];
                 sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
@@ -152,21 +182,25 @@ module.exports = function () {
             }res.redirect('/transactions');
         });
     });
-    router.post('/search_transactions', function (req, res) {
-        //console.log(req.body.homeworld)
-        console.log(req.body)
-        var mysql = req.app.get('mysql');
-        var sql = "(SELECT tr.id AS 'ID', tr.cname AS 'Name', an.type AS 'Item' FROM transaction_table tr INNER JOIN adoption_table ad ON tr.id = ad.tid INNER JOIN animal_table an ON ad.aid = an.id WHERE cname = ?) UNION ALL (SELECT tr2.id AS 'ID', tr2.cname AS 'Name', su.item AS 'Item' FROM transaction_table tr2 INNER JOIN purchase_table pr ON tr2.id = pr.tid INNER JOIN supply_table su ON pr.sid=su.id WHERE cname = ?)";
-        var inserts = [req.cname, req.cname];
-        sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
-            if (error) {
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            res.redirect('/transactions');
-        });
-    });
+    // router.get('/search_transactions/', function (req, res) {
+    //     console.log("search button function thingy yay")
+    //     //console.log(req.body.homeworld)
+    //     console.log(req.body)
+    //     var mysql = req.app.get('mysql');
+    //     var sql = "(SELECT tr.id AS 'ID', tr.cname AS 'Name', an.type AS 'Item' FROM transaction_table tr INNER JOIN adoption_table ad ON tr.id = ad.tid INNER JOIN animal_table an ON ad.aid = an.id WHERE cname = ?) UNION ALL (SELECT tr2.id AS 'ID', tr2.cname AS 'Name', su.item AS 'Item' FROM transaction_table tr2 INNER JOIN purchase_table pr ON tr2.id = pr.tid INNER JOIN supply_table su ON pr.sid=su.id WHERE cname = ?)";
+    //     var inserts = [req.cname, req.cname];
+    //     sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+    //         if (error) {
+    //             console.log(JSON.stringify(error))
+    //             res.write(JSON.stringify(error));
+    //             res.end();
+    //         }
+    //         res.redirect('/transactions');
+    //     });
+    // });
+
+
+
     router.post('/returnsupply', function (req, res) {
         //console.log(req.body.homeworld)
         console.log(req.body)
